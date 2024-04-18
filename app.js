b58a02dfc26ab41e4972bc992305106a99165538
Rushabh Babre <babrerushabh1@gmail.com>
2024-04-19 03:59:40 +0530

    schemas

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+//------Main App------//
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
---
+++ models/users.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age:{
+    type: Number,
+    default: 0,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  work_details: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
