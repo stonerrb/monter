73ccc10f408b616aa1e6c92498cf894b41940268
Rushabh Babre <babrerushabh1@gmail.com>
2024-04-20 16:27:36 +0530

    test

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const authenticateToken = require('../middleware/auth');
 
 const router = express.Router();
 
+//--all routes are working fine--//
 router.post('/signup', userSignup);
 router.post('/verifyOTP', OTPverify);
 router.post('/add_info', addInfo);
